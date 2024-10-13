b102de04eba19978d14493d5d53d823cd1e4a8fd
Nour Edres <158815032+NourMoveo@users.noreply.github.com>
2024-10-13 09:09:32 +0300

    add load tasks in create game

--- src/view/pages/Games/AddUnit/ChoosableTask/ChooseTask.tsx
+++ src/view/pages/Games/AddUnit/ChoosableTask/ChooseTask.tsx
@@ -5,6 +5,7 @@ import HomePage from "../../../../components/Common/HomePage/HomePage";
 import { Task } from "../../../../../redux/models/Interfaces";
 import { useDispatch } from "react-redux";
 import { setIsCreateGame } from "../../../../../redux/slices/GlobalStates";
+import Loader from "../../../../components/Common/LoadingSpinner/Loader";
 
 interface ChoosableTasksPageProps {
   fromParent: string;
@@ -12,27 +13,47 @@ interface ChoosableTasksPageProps {
 
 const ChoosableTasksPage: FC<ChoosableTasksPageProps> = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [loadingMessage, setLoadingMessage] = useState<string>("");
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(setIsCreateGame(true));
     const fetchTasks = async () => {
-      const fetchedTasks = await taskAPI.getAllTasks();
-      setTasks(fetchedTasks);
+      setIsLoading(true);
+      setLoadingMessage("טוען משימות...");
+
+      try {
+        const fetchedTasks = await taskAPI.getAllTasks();
+        setTasks(fetchedTasks);
+      } catch (error) {
+        console.error("Failed to fetch tasks:", error);
+        setLoadingMessage("שגיאה בטעינת משימות");
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchTasks();
-  }, []);
+  }, [dispatch]);
 
   return (
-    <HomePage
-      objects={tasks}
-      page="ChooseTask"
-      Component={(props) => (
-        <ChoosableTaskCard {...props} navigationPath={"/TaskDetailsAddGame"} />
-      )}
-      addButton="הוספת משימה חדשה"
-      addButtonPath="addTask"
-      setCardOnClick={false}
-    />
+    <div dir="rtl">
+      <Loader isLoading={isLoading} message={loadingMessage} />
+
+      <HomePage
+        objects={tasks}
+        page="ChooseTask"
+        Component={(props) => (
+          <ChoosableTaskCard
+            {...props}
+            navigationPath={"/TaskDetailsAddGame"}
+          />
+        )}
+        addButton="הוספת משימה חדשה"
+        addButtonPath="addTask"
+        setCardOnClick={false}
+      />
+    </div>
   );
 };
 
