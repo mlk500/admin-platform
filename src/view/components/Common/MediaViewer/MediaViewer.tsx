65f69f51896935b430f1f89b5a58db9a3f1b95f1
Nour Edres <158815032+NourMoveo@users.noreply.github.com>
2024-10-29 11:18:47 +0200

    fix add task

--- src/view/components/Common/MediaViewer/MediaViewer.tsx
+++ src/view/components/Common/MediaViewer/MediaViewer.tsx
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MediaTask } from "../../../../redux/models/Interfaces";
 import PDFViewer from "../PDFViewer/PDFViewer";
 import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/src/styles.scss";
 import ReactPlayer from "react-player";
+import "react-h5-audio-player/src/styles.scss";
 import "./MediaViewer.scss";
 
+interface MediaTaskTBC {
+  mediaTaskID: string;
+  mediaType: string;
+  mediaUrl?: string;
+  fileName: string;
+}
+
 interface MediaViewerProps {
-  mediaList: MediaTask[];
+  mediaList: MediaTaskTBC[] | MediaTask[];
   onDelete?: (index: number) => void;
   deletable?: boolean;
+  maxMediaCount?: number;
+  onUploadRestricted?: (message: string) => void;
 }
 
 const MediaViewer: React.FC<MediaViewerProps> = ({
   mediaList,
   onDelete,
   deletable = false,
+  maxMediaCount = 100,
+  onUploadRestricted,
 }) => {
+  const [allowedMediaList, setAllowedMediaList] = useState<
+    (MediaTaskTBC | MediaTask)[]
+  >([]);
+
+  useEffect(() => {
+    if (mediaList.length > 0) {
+      const hasImage = mediaList.some((media) =>
+        media.mediaType.includes("image")
+      );
+      const hasVideo = mediaList.some((media) =>
+        media.mediaType.includes("video")
+      );
+
+      if (hasImage && hasVideo && onUploadRestricted) {
+        onUploadRestricted("צריך להוסיף תמונה או סרטון , אי אפשר לבחור בשניהם");
+      }
+
+      setAllowedMediaList(mediaList.slice(0, maxMediaCount));
+    }
+  }, [mediaList, maxMediaCount, onUploadRestricted]);
+
   return (
     <div className="media-grid">
-      {mediaList.map((media, index) => (
+      {allowedMediaList.map((media, index) => (
         <div key={media.mediaTaskID} className="media-item">
           {media.mediaType.includes("application/pdf") ? (
             media.mediaUrl ? (
